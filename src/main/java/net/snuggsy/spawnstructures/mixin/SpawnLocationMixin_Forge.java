c2fcd60785bfc5818b386273e78c0b0b1c97623b
Snugs <albinowolfgaming@outlook.com>
2024-01-19 18:38:39 +1000

    Fix critical error: Player data corrupted on server restart

--- src/main/java/net/snuggsy/spawnstructures/mixin/SpawnLocationMixin_Forge.java
+++ src/main/java/net/snuggsy/spawnstructures/mixin/SpawnLocationMixin_Forge.java
@@ -5,7 +5,6 @@ import net.minecraft.server.level.ServerPlayer;
 import net.snuggsy.spawnstructures.SpawnStructures;
 import net.snuggsy.spawnstructures.events.StructureSpawnEvent;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
