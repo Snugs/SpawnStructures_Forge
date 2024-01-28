9bb4c8ddd1561e7068e40bf6d605b677350777a3
Snugs <albinowolfgaming@outlook.com>
2024-01-28 21:17:20 +1000

    Implement world centre spawning

--- src/main/java/net/snuggsy/spawnstructures/util/StructureCoordinates.java
+++ src/main/java/net/snuggsy/spawnstructures/util/StructureCoordinates.java
@@ -4,6 +4,14 @@ import net.minecraft.core.BlockPos;
 
 public class StructureCoordinates {
 
+    public BlockPos getStructureSize() {
+        return new BlockPos(0,0,0);
+    }
+
+    public BlockPos getStructureOffset() {
+        return new BlockPos(0,0,0);
+    }
+
     // With the Structure Block placed directly the spawn point of the Starter Structure,
     // the following values are the bounding box offset and bounding box size of said Starter Structure.
 
