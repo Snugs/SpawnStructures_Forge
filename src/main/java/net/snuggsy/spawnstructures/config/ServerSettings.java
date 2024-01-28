914e1dba7f243623620f804cfc9b1e1916c79369
Snugs <albinowolfgaming@outlook.com>
2024-01-28 20:43:55 +1000

    Prep for Config

---
+++ src/main/java/net/snuggsy/spawnstructures/config/ServerSettings.java
@@ -0,0 +1,23 @@
+package net.snuggsy.spawnstructures.config;
+
+public class ServerSettings {
+
+    public static boolean spawnWorldCentre = true;
+
+    public static int spawnRadius = 0; // Options: -1 < spawnRadius < 4
+
+    public static String spawnOrientation = "STRUCTURE_LOCKED"; // Options: STRUCTURE_LOCKED (Facing the door), RANDOMIZED, NORTH, EAST, SOUTH, WEST
+
+    public static String biomeSelected = "ANY"; // Options: ANY, (A list of all loaded biomes)
+
+    public static boolean biomeDependantStructures = true;
+
+    public static String structureSelected = "BIOME_DEPENDANT"; // Options: BIOME_DEPENDANT, CHERRY_BLOSSOM
+
+    public static String substructure = "SUBSTRATE"; // Options: SUBSTRATE, MAP_ROOM
+
+    public static boolean ignoreGameruleGenStructures = true;
+
+    public static boolean ignoreGameruleSpawnRadius = true;
+
+}
