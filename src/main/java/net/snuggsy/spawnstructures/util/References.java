32ea7a3865e86d2ec618103a7085d649b2b64c8e
Snugs <albinowolfgaming@outlook.com>
2024-02-05 19:10:22 +1000

    bump to 1.0.7

--- src/main/java/net/snuggsy/spawnstructures/util/References.java
+++ src/main/java/net/snuggsy/spawnstructures/util/References.java
@@ -3,6 +3,6 @@ package net.snuggsy.spawnstructures.util;
 public class References {
     public static final String MOD_ID = "spawnstructures_forge";
     public static final String NAME = "Spawn Structures";
-    public static final String VERSION = "1.0.6";
+    public static final String VERSION = "1.0.7";
     public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
