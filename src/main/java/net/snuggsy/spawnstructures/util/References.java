9497b684d2dbb03e8376fdb0726301639ed7595a
Snugs <albinowolfgaming@outlook.com>
2024-02-15 09:50:21 +1000

    Add a config version reference

--- src/main/java/net/snuggsy/spawnstructures/util/References.java
+++ src/main/java/net/snuggsy/spawnstructures/util/References.java
@@ -5,4 +5,5 @@ public class References {
     public static final String NAME = "Spawn Structures";
     public static final String VERSION = "1.0.7";
     public static final String ACCEPTED_VERSIONS = "[1.20.1]";
+    public static final String CONFIG_VERSION = "1.0.4";
 }
